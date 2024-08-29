42341fd5f4afd0e589a939193f3213542d650347
Johnny <nguyen.johnly25>
2024-08-29 12:53:04 -0700

    fix time

--- db/queries.js
+++ db/queries.js
@@ -4,7 +4,8 @@ exports.getAllMessages = async () => {
     const { rows } = await pool.query(
         `SELECT *, to_char(date, 'YYYY-MM-DD HH24:MI') as date_formatted,
         to_char(date, 'YYYY-MM-DD') as date_only,
-        to_char(date, 'HH24:MI') as time_only
+        to_char(date, 'HH24:MI') as time_only,
+        to_char(date AT TIME ZONE 'UTC' AT TIME ZONE 'US/PACIFIC', 'HH24:MI') as time_pacific
          FROM messages`
     );
     return rows;
